19b8e569f74b8e86d6cf32ea8c0f5f4ce78f1fe8
sydney-williams <70979348+sydney-williams@users.noreply.github.com>
2021-01-17 19:26:33 -0600

    Upload starter code.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Sorting functions (invoked by sortTable())
+ufoTable(tableData)
+
+function ufoTable(ufoData){
+    var table = document.getElementsByClassName("table table-striped")
+
+    for (var i = 0; i < ufoData.length; i++)
+    {
+        var row = <tr>
+                    <td>$(ufoData[i].datetime)</td>
+                    <td>$(ufoData[i].city)</td>
+                    <td>$(ufoData[i].state)</td>
+                    <td>$(ufoData[i].country)</td>
+                    <td>$(ufoData[i].shape)</td>
+                    <td>$(ufoData[i].durationMinutes)</td>
+                    <td>$(ufoData[i].comments)</td>
+                        
+        </tr>
+        table.innerHTML += row
+    }    
+}    
